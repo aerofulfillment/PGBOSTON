4c5598591f11f8e1016541dafa47caf0456ae9f5
Ben Yurchison <yurchisonben@gmail.com>
2019-06-04 15:34:32 -0400

    creating model for enrollment data
    creating model for enroll data that can be seen or input from employee and also updated/ modified later by admin. also removing base site creation page asp.net fluff

---
+++ PGBOSTON/Models/EnrollmentData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PGBOSTON.Models
+{
+    public class EnrollmentData
+    {
+        public int Id { get; set; }
+        public string Employee { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime RequestEventDate { get; set; }
+        public string HomeAddress { get; set; }
+        public int RequestMoreTickets { get; set; }
+        public int MaxTicketCount { get; }
+        public Boolean RequireTransportation { get; set; }
+        public Boolean Retiree { get; set; }
+
+    }
+}
